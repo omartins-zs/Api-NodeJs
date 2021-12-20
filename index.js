03fcaed65181b8ca624f247d147ce3b487dbc027
omartins-zs <gabrielmatheus209@gmail.com>
2021-12-20 08:49:40 -0300

    Instalando Consign pelo NPM

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
+const consign = require("consign");
+
 app.use(express.json());
+
+consign().then("./api").then("./routes/routes.js").into("app");
 
 // GET
 app.get("/users", function (req, res) {
