b7c21f7ce7406420908d8623befdece469bc8b37
omartins-zs <gabrielmatheus209@gmail.com>
2021-12-23 11:23:35 -0300

    Configurando banco de dados no Js e Projeto

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const app = express();
 const consign = require("consign");
+const database = require("./config/database");
 
 app.use(express.json());
+app.database = database;
 
-consign().then("./api").then("./routes/routes.js").into("app");
-
-// GET
-app.get("/users", function (req, res) {
-  const users = [
-    {
-      id: 1,
-      name: "Usuario Jorge",
-    },
-    {
-      id: 2,
-      name: "Usuario Fernando",
-    },
-  ];
-
-  return res.json(users);
-});
-
-// POST
-app.post("/users", function (req, res) {
-  const user = { ...req.body };
-
-  return res.json(user);
-});
+consign().then("./api").then("./routes/routes.js").into(app);
 
 // PUT
 app.put("/users/:id", function (req, res) {
