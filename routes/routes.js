c6364fa715032e6d9e5ed0ce5c1df4c0ed495fa5
omartins-zs <gabrielmatheus209@gmail.com>
2022-01-03 13:24:42 -0300

    Adicionando Routes Get no Categories

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,8 @@
 module.exports = (app) => {
   app.route("/users").get(app.api.users.get).post(app.api.users.save);
 
-  app.route("/categories").post(app.api.categories.save);
+  app
+    .route("/categories")
+    .get(app.api.categories.get)
+    .post(app.api.categories.save);
 };
